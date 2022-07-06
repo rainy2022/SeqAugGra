9de051c6fce6a1c80351fe27de9bf0deb37a990b
rainy2022 <108351756+rainy2022@users.noreply.github.com>
2022-07-06 14:58:54 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+  name = 'auglichem',
+  packages=setuptools.find_packages(exclude=("docs","examples",)), 
+  version = '0.1.1',
+  license = 'MIT',
+  description = 'Data augmentation of molecules and crystals.',
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  author = 'Yuyang Wange, Rishikesh Magar, Cooper Lorsung, Hariharan Ramasubramanian, Chen Liang, Peiyuan Li, Amir Barati Farimani',
+  author_email = 'clorsung@andrew.cmu.edu',
+  url = 'https://github.com/BaratiLab/AugLiChem',
+  classifiers = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    "Operating System :: OS Independent",
+  ],
+  python_requires = '>=3.8',
+  install_requires = ['numpy>=1.21.1',
+                      'pytest>=6.0.1',
+                      'pytest-cov',
+                      'sklearn',
+                      'pandas>=1.3.1',
+                      'matplotlib>=3.4.2',
+                      'pyyaml>=5.4.1',
+                      'tqdm',
+                      'ase>=3.22.0',
+                      'pymatgen>=2022.0.11',
+                      'rdflib',
+                      'rdkit-pypi',
+                      'tensorboard>=2.4.1',
+                     ],
+)
